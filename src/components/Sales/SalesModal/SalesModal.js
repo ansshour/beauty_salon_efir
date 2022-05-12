307f228e296303e4c4c3a4a6bab06421d801d1f0
ansshour <andrey.zhukov001@gmail.com>
2022-05-12 17:11:20 +0300

    email send

--- src/components/Sales/SalesModal/SalesModal.js
+++ src/components/Sales/SalesModal/SalesModal.js
@@ -1,27 +1,30 @@
 import React, { useState } from "react";
 import styles from "./SalesModal.module.css";
 import { Form, Button } from "react-bootstrap";
+import emailjs from "emailjs-com";
 
 export const SalesModal = ({ modalActive, setModalActive, modalData }) => {
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm("service_2fog2mq", "template_owkgzk4", e.target, "IT8ug2LgzJmA6IvGA").then(res => {
+            console.log(res)
+        }).catch(err => console.log(err))
+    }
 
 
     return (
         <div className={modalActive ? `${styles.container} ${styles.active}` : `${styles.container}`} onClick={() => { setModalActive(false) }}>
             <div className={modalActive ? `${styles.modal} ${styles.modalActive}` : `${styles.modal}`} onClick={e => e.stopPropagation()}>
                 <p style={{ fontWeight: "bold", marginBottom: "10px" }}>Название услуги: {modalData.title}</p>
-                <Form>
-                    <Form.Group className="mb-2">
-                        <Form.Label style={{ marginBottom: "1px" }}>Ваше ФИО</Form.Label>
-                        <Form.Control className={styles.input} type="name" placeholder="Иванов Иван Иванович" />
-                    </Form.Group>
-                    <Form.Group className="mb-4">
-                        <Form.Label style={{ marginBottom: "1px" }}>Ваш номер телефона</Form.Label>
-                        <Form.Control className={styles.input} type="phone" placeholder="+7 (999) 999 999" />
-                    </Form.Group>
-                    <Button variant="dark" type="submit">
-                        Участвовать в акции
-                    </Button>
-                </Form>
+                <form onSubmit={sendEmail}>
+                    <label>ФИО</label>
+                    <input type="text" name="name" />
+                    <label>Телефон</label>
+                    <input type="phone" name="phone" />
+                    <input type="submit" value="Участвовать в акции" />
+                </form>
             </div>
         </div>
     )
